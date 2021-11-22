376c65f86ff3825b60863423aaed33d0bfb02818
felipesantosds <felipedssantos.dev@gmail.com>
2021-11-22 12:29:08 -0300

    Add responsivity

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -1,5 +1,7 @@
 import { createRef, useEffect } from "react";
 
+import { BiMenu } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
 import { ImHome } from "react-icons/im";
 import { BiSearch } from "react-icons/bi";
 import { BsFillPersonFill, BsFillGrid3X3GapFill } from "react-icons/bs";
@@ -10,46 +12,82 @@ import MenuItem from "./MenuItem";
 
 function Header() {
     const headerRef = createRef<HTMLElement>();
+    const menuRef = createRef<HTMLDivElement>();
+    let isShowMenu = false;
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
             if (headerRef.current) {
-                if (document.body.scrollTop > 100 
+                if (document.body.scrollTop > 100
                     || document.documentElement.scrollTop > 100) {
                     headerRef.current.style.backgroundColor = "white";
                     headerRef.current.style.boxShadow = "0 0 3px rgba(0, 0, 0, 0.25)";
                 } else {
                     headerRef.current.style.backgroundColor = "transparent";
-                    headerRef.current.style.boxShadow = "none"
+                    headerRef.current.style.boxShadow = "none";
                 }
             }
         });
         // eslint-disable-next-line
     }, []);
 
+    const mql = window.matchMedia("(max-width: 600px)");
+
+    function toggleMenu(event: MediaQueryListEvent) {
+        if (event.matches) {
+            if (menuRef.current) {
+                menuRef.current.style.display = "none";
+                isShowMenu = false;
+            }
+        } else {
+            if (menuRef.current) {
+                menuRef.current.style.display = "flex";
+                isShowMenu = true;
+            }
+        }
+    }
+
+    mql.addEventListener("change", toggleMenu);
+
+    function onClickToggleMenu() {
+        if (menuRef.current) {
+            if (isShowMenu) {
+                menuRef.current.style.display = "none";
+                isShowMenu = false;
+            } else {
+                menuRef.current.style.display = "flex";
+                isShowMenu = true;
+            }
+        }
+    }
+
     return (
         <header ref={headerRef} className={styles.container}>
             <div>
-                <MenuItem
-                    href="#home"
-                    Icon={ImHome}
-                    text="Home"
-                />
-                <MenuItem
-                    href="#about"
-                    Icon={BsFillPersonFill}
-                    text="About"
-                />
-                <MenuItem
-                    href="#portfolio"
-                    Icon={BsFillGrid3X3GapFill}
-                    text="Portfolio"
-                />
-                <MenuItem
-                    href="#contact"
-                    Icon={MdMail}
-                    text="Contact"
-                />
+                <BiMenu onClick={onClickToggleMenu} />
+                <div ref={menuRef}>
+                    <IoClose onClick={onClickToggleMenu} />
+                    <MenuItem
+                        href="#home"
+                        Icon={ImHome}
+                        text="Home"
+                    />
+                    <MenuItem
+                        href="#about"
+                        Icon={BsFillPersonFill}
+                        text="About"
+                    />
+                    <MenuItem
+                        href="#portfolio"
+                        Icon={BsFillGrid3X3GapFill}
+                        text="Portfolio"
+                    />
+                    <MenuItem
+                        href="#contact"
+                        Icon={MdMail}
+                        text="Contact"
+                    />
+                </div>
             </div>
             <button>
                 <BiSearch />
